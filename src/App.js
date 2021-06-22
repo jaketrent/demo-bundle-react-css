5ea0e258d569f74cd2bf45882d79c0c8f700beac
jaketrent <trent.jake@gmail.com>
2021-06-22 14:36:47 -0600

    integrate css in js ds component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import Button from '@pluralsight/ps-design-system-button'
 import React from 'react'
 
 export default function App() {
-  return <div>My app</div>
+  return (
+    <div>
+      My app<Button>Button that has CSS in JS</Button>
+    </div>
+  )
 }
